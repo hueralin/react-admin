fcbd8096bac877b1d484c70c7e75679854063355
malin <1612415787@qq.com>
2020-06-07 22:11:55 +0800

    搭建整体界面结构

--- src/pages/admin/Admin.jsx
+++ src/pages/admin/Admin.jsx
@@ -4,18 +4,35 @@
 import React, { Component } from 'react'
 import { Redirect } from 'react-router-dom'
 import memoryUtil from '../../utils/memoryUtil'
+import { Layout } from 'antd'
+import Header from '../../components/header'
+import LeftNav from '../../components/left-nav'
+
+const { Footer, Sider, Content } = Layout
 
 export default class Admin extends Component {
     render() {
-        const { userInfo } = memoryUtil
-        if (!userInfo || !userInfo._id) {
-            // 未登录
-            return <Redirect to='/login' />
-        }
+        // const { userInfo } = memoryUtil
+        // if (!userInfo || !userInfo._id) {
+        //     // 未登录
+        //     return <Redirect to='/login' />
+        // }
+        // return (
+        //     <div>
+        //         Hello { userInfo.username } !
+        //     </div>
+        // )
         return (
-            <div>
-                Hello { userInfo.username } !
-            </div>
+            <Layout style={{ height: '100%' }}>
+                <Sider>
+                    <LeftNav></LeftNav>
+                </Sider>
+                <Layout>
+                    <Header>头部</Header>
+                    <Content style={{ backgroundColor: '#fff' }}>主体</Content>
+                    <Footer style={{ textAlign: 'center', color: '#aaa' }}>&copy;版权所有</Footer>
+                </Layout>
+            </Layout>
         )
     }
 }
