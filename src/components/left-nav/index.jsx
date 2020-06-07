95a0a0104422b6b5650cdc3b6dc6e7076a106cd2
malin <1612415787@qq.com>
2020-06-08 00:05:13 +0800

    动态渲染侧边菜单

--- src/components/left-nav/index.jsx
+++ src/components/left-nav/index.jsx
@@ -1,10 +1,52 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
 import { Menu } from 'antd'
+import menuList from '../../config/menuConf'
 import './style.less'
 import logo from '../../assets/imgs/logo.png'
 
 const SubMenu = Menu.SubMenu
+
+// 动态渲染菜单列表（map + 递归）
+function renderMenu_map(menuList) {
+    // 返回数组
+    return menuList.map(item => {
+        if (!item.children) {
+            return (
+                <Menu.Item key={item.key.slice(1)}>
+                    <Link to={item.key}>
+                        <span>{item.title}</span>
+                    </Link>
+                </Menu.Item>
+            )
+        }
+        return <SubMenu key={item.key.slice(1)} title={item.title}>
+            { renderMenu_map(item.children) }
+        </SubMenu>
+    })
+}
+// 动态渲染菜单列表（reduce + 递归）
+function renderMenu_reduce(menuList) {
+    // 返回数组
+    return menuList.reduce((prev, item) => {
+        if (!item.children) {
+            prev.push((
+                <Menu.Item key={item.key.slice(1)}>
+                    <Link to={item.key}>
+                        <span>{item.title}</span>
+                    </Link>
+                </Menu.Item>
+            ))
+        } else {
+            prev.push((
+                <SubMenu key={item.key.slice(1)} title={item.title}>
+                    { renderMenu_reduce(item.children) }
+                </SubMenu>
+            ))
+        }
+        return prev
+    }, [])
+}
 
 export default class LeftNav extends Component {
     render() {
@@ -20,50 +62,7 @@ export default class LeftNav extends Component {
                     defaultSelectedKeys={['1']}
                     // 子菜单垂直收缩
                     mode='inline'>
-                    <Menu.Item key='1'>
-                        <Link to='/'>
-                            <span>首页</span>
-                        </Link>
-                    </Menu.Item>
-                    <SubMenu key='goods' title='商品'>
-                        <Menu.Item key='goods-1'>
-                            <Link to='/category'>
-                                <span>品类管理</span>
-                            </Link>
-                        </Menu.Item>
-                        <Menu.Item key='goods-2'>
-                            <Link to='/product'>
-                                <span>商品管理</span>
-                            </Link>
-                        </Menu.Item>
-                    </SubMenu>
-                    <Menu.Item key='3'>
-                        <Link to='/user'>
-                            <span>用户管理</span>
-                        </Link>
-                    </Menu.Item>
-                    <Menu.Item key='4'>
-                        <Link to='/role'>
-                            <span>角色管理</span>
-                        </Link>
-                    </Menu.Item>
-                    <SubMenu key='graph' title='图形图表'>
-                        <Menu.Item key='graph-1'>
-                            <Link to='/chart/bar'>
-                                <span>柱形图</span>
-                            </Link>
-                        </Menu.Item>
-                        <Menu.Item key='graph-2'>
-                            <Link to='/chart/line'>
-                                <span>折线图</span>
-                            </Link>
-                        </Menu.Item>
-                        <Menu.Item key='graph-3'>
-                            <Link to='/chart/pie'>
-                                <span>饼图</span>
-                            </Link>
-                        </Menu.Item>
-                    </SubMenu>
+                    { renderMenu_reduce(menuList) }
                 </Menu>
             </div>
         )
